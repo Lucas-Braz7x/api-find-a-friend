a4a6fc9ee13bad217aaff07f34df291d784c42ac
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2025-08-12 17:15:57 -0300

    :tada: configuração inicial

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  PORT: z.coerce.number().default(3000),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error(
+    "Oh, no!! As variáveis de ambiente não foram configuradas",
+    _env.error.format()
+  );
+
+  throw new Error("Oh, no!! As variáveis de ambiente nãop foram configuradas");
+}
+
+export const env = _env.data;
