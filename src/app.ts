24262c299aca7b2ac9dfb1bea4c51bddceb0d4a2
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2025-08-13 15:44:26 -0300

    :wrench: chore: adicionando middlewares de autenticação

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,24 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import { appRoutes } from './http/routes';
+import fastifyCookie from '@fastify/cookie';
+import fastifyJwt from '@fastify/jwt';
 
 export const app = fastify();
+
+app.register(fastifyCookie);
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
 
 app.register(appRoutes);
 
